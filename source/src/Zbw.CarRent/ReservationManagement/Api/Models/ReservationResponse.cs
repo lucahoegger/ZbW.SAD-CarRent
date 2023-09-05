dcb41080245ef328687413dd047bec424d5ecd7c
Luca <luhoe1999@gmail.com>
2023-09-05 22:34:03 +0200

    wip

--- source/src/Zbw.CarRent/ReservationManagement/Api/Models/ReservationResponse.cs
+++ source/src/Zbw.CarRent/ReservationManagement/Api/Models/ReservationResponse.cs
@@ -1,12 +1,9 @@
-using Zbw.Carrent.CarManagement;
-using Zbw.Carrent.CustomerManagement.Api.Models;
-
 namespace Zbw.Carrent.ReservationManagement.Api.Models;
 
 public record ReservationResponse(
-        Guid Id,
-        int AmountOfDays,
-        decimal TotalCosts,
-        CarResponse Car,
-        CustomerResponse Customer
-    );
+    Guid Id,
+    int AmountOfDays,
+    decimal TotalCosts,
+    Guid CarId,
+    Guid CustomerId
+);
