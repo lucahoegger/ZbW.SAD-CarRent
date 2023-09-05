ee92db1f302db8c1a3eeac9b2a88936627d059f0
Luca <luhoe1999@gmail.com>
2023-09-05 22:33:49 +0200

    wip

---
+++ source/src/Zbw.CarRent/CrossCutting/Infrastructure/Persistence/Migrations/20230905201107_added_rentalContract.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Zbw.Carrent.CrossCutting.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class added_rentalContract : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RentalContracts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ReservationId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RentalContracts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RentalContracts_Reservations_ReservationId",
+                        column: x => x.ReservationId,
+                        principalTable: "Reservations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RentalContracts_ReservationId",
+                table: "RentalContracts",
+                column: "ReservationId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RentalContracts");
+        }
+    }
+}
